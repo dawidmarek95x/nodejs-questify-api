97a7ecb74e9fb56c71e7c6d6092f3b4256963541
dawidmarek95x <dawid.marek95x@gmail.com>
2022-12-19 16:03:11 +0100

    Created API documentation by swagger

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const apiDocsRouter = require("./api-docs");
 const usersRouter = require("./auth");
 const contactsRouter = require("./card");
 
+router.use('/api-docs', apiDocsRouter);
 router.use("/auth", usersRouter);
 router.use("/card", contactsRouter);
 
